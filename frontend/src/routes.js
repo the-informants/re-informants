b2f14a7a621b80d383b8d6d6573ceb5d96a3dc2d
kellerchch <kellerchch@gmail.com>
2018-06-06 19:39:58 -0600

    updated routes with page shells, added link to bootstraps styles, deleted index.css and consolidated custom styles into app.css

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,10 +2,20 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Login from './components/Intro/About';
+import Account from './components/Intro/AccountProfile';
+import Buyer from './components/Intro/Buyer';
+import GetStarted from './components/Intro/GetStarted';
+import Informant from './components/Intro/Informant';
+import InquiryThankYou from './components/Intro/InquiryThankYou';
 
 export default (
     <Switch>
         <Route exact path = '/' component={Login}/>
+        <Route component={Account} path='/Account' />
+        <Route component={Buyer} path='/Buyer' />
+        <Route component={GetStarted} path='/GetStarted' />
+        <Route component={Informant} path='/Informant' />
+        <Route component={InquiryThankYou} path='/ThankYou' />
      
     </Switch>
 )
