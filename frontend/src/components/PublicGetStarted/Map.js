1d6d80c175bada0876abe87f45d1d05ea2e9ebf8
Shane Tidwell <shane.tidwell21@gmail.com>
2018-06-19 16:43:26 -0600

    interactive colored markers and add to cart

--- frontend/src/components/PublicGetStarted/Map.js
+++ frontend/src/components/PublicGetStarted/Map.js
@@ -1,9 +1,13 @@
+/*global google*/
 import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps"
 import React, {Component} from 'react';
 import '../../App.css';
 import {connect} from 'react-redux';
 import {mapMoveCoordinates, searchAddress, addSelectedInformant} from '../../ducks/reducers/search'
+import mapMarker from '../../map-marker.svg'
+import greenMarker from '../../greenMarker.svg'
+import redMarker from '../../redMarker.svg'
 
 class GoogleMaps extends Component {
   constructor(){
@@ -36,7 +40,7 @@ class GoogleMaps extends Component {
   }
   handleClick = (informant)=>{
     this.props.addSelectedInformant(informant.informantid)
-    console.log("click", informant)
+    // console.log("click", informant)
   }
   
     render (){
@@ -48,9 +52,13 @@ class GoogleMaps extends Component {
             lng: parseFloat(informant.lng)
           }
         }
-        return <Marker onClick={()=>this.handleClick(informant)} key={i}{...marker}/>
+        if(informant.informantid===this.props.search.selectedInformantId){
+          return <Marker onClick={()=>this.handleClick(informant)} icon={{url: greenMarker, scaledSize: {width: 40, height: 40}}} key={i}{...marker}/>
+        } else {
+          return <Marker onClick={()=>this.handleClick(informant)} icon={{url: mapMarker, scaledSize: {width: 40, height: 40}}} key={i}{...marker}/>
+        }
       })
-      // console.log("props", this.props)
+      
         return(
         <div>
             <GoogleMap
@@ -59,7 +67,7 @@ class GoogleMaps extends Component {
               defaultZoom={14}
               center={{lat: this.props.search.mapMoveLat||this.props.center.lat||this.props.defaultCenter.lat, lng: this.props.search.mapMoveLng||this.props.center.lng||this.props.defaultCenter.lng}}
               defaultCenter={{ lat: 41.00472, lng: -111.9051596 }}>
-              {this.props.search.searchLat!==null && <Marker position={{lat:this.props.search.searchLat, lng: this.props.search.searchLng}}/>}              
+              {this.props.search.searchLat!==null && <Marker position={{lat:this.props.search.searchLat, lng: this.props.search.searchLng}} icon={{url: redMarker, scaledSize: {width: 40, height: 40}}}/>}              
               {markers}
               
              
@@ -72,7 +80,7 @@ function mapStateToProps(state){
   const {search, form} = state
   return {search, form}
 }
-export default connect(mapStateToProps,{mapMoveCoordinates, searchAddress, addSelectedInformant})(withGoogleMap(GoogleMaps))
+export default connect(mapStateToProps,{mapMoveCoordinates, searchAddress, addSelectedInformant})(withScriptjs(withGoogleMap(GoogleMaps)))
 
 
 // export default withGoogleMap(GoogleMaps)
