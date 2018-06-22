fad1fc2c0cdeae682b486a89cc026ba0762d5915
Lin Wang <lin.wang@healthcatalyst.com>
2018-06-21 19:09:01 -0600

    set avg

--- backend/db/searchinformants_by_lat_lng.sql
+++ backend/db/searchinformants_by_lat_lng.sql
@@ -1,4 +1,9 @@
-select *, (point(i.lng,i.lat) <@> point(${lng},${lat})) as distance
+select i.*, avgstarrating, (point(i.lng,i.lat) <@> point(${lng},${lat})) as distance
 from informants i
+LEFT JOIN ( SELECT informantid, AVG(starrating) as avgstarrating
+            FROM informantreview
+            GROUP BY informantid
+            ) ir
+            on i.informantid::integer = ir.informantid::integer
 where (point(i.lng,i.lat) <@> point(${lng},${lat})) < 50
 order by distance 
