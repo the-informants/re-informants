4879668486b7320dda3f3caf11a4155b61f24d6a
kellerchch <kellerchch@gmail.com>
2018-06-23 14:01:09 -0600

    style changes across each of the pages where we are displaying data

--- frontend/src/components/PrivateInformant/Informant.js
+++ frontend/src/components/PrivateInformant/Informant.js
@@ -13,33 +13,61 @@ class PrivateInformant extends Component {
     render (){
         return(
             <div className="container PageTitle">
-                <h1>Private Informant</h1>
+                <h1>Informant</h1>
                 {this.props.order.orderResultsbyInformant[0]
-                        ?<div>here are your inquiries:
+                        ?<div>
+                            <h4>Here are your inquiries:</h4>
+
                             {this.props.order.orderResultsbyInformant.map((inquiry) => {
 
                             return (
-                            <div>
-                                Inquiry Name: {inquiry.ordername}
-                                Inquiry Address: {inquiry.address}
-                                Inquiry Type: {inquiry.ordertype}
-                                Inquiry Notes: {inquiry.ordernotes}
-                                Inquiry Timestamp: {inquiry.orderdatetime}
-                                Inquiry Distance: {inquiry.distance}
-                                Inquiry Status: {inquiry.orderresultstatus}
+                                <div className="container">
+                                <div className="order"> 
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Name:</dt>
+                                        <dd>{inquiry.ordername}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Address:</dt>
+                                        <dd>{inquiry.address}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Type:</dt>
+                                        <dd> {inquiry.ordertype}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Notes:</dt>
+                                        <dd>{inquiry.ordernotes}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Time Stamp:</dt>
+                                        <dd>{inquiry.orderdatetime}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Distance:</dt>
+                                        <dd>{inquiry.distance}</dd>
+                                    </dl>
+                                    <dl className="dl-horizontal">
+                                        <dt>Inquiry Status:</dt>
+                                        <dd>{inquiry.orderresultstatus}
+                                        </dd>
+                                    </dl>
+                                </div>
                             </div>
                             )
                             })
                             }
                         </div>
-                        :   <div>You don't have any inquiry</div>
+                        :   <div className="container">
+                                <h4>You don't have any inquiries
+                                </h4>
+                                <Link to="/">
+                                <button className="btn btn-primary">
+                                    Search for a Buyer
+                                </button>
+                                </Link>
+                            </div>
                 }
-
-                <Link to="/">
-                    <button className="btn btn-primary">
-                        Search for a buyer
-                    </button>
-                </Link>
             </div>
         )
     }
